1d9f70508d2992619ba10961227bff2092d67ea0
Sean Yap <syap0317@gmail.com>
2022-03-09 00:48:55 -0500

    added modal popup for add task functionality

--- index.js
+++ index.js
@@ -3,28 +3,37 @@ const doneContainer = document.querySelector("#done ul");
 const checkboxes = document.querySelectorAll(
   "input[type=checkbox][name=checkbox]"
 );
+const addTaskBtn = document.getElementById("addTask");
+const todoList = document.getElementById("todoList");
 
-//add event listeners to all list input
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", (event) => {
-    if (event.target.checked) {
-      doneContainer.appendChild(event.target.parentElement);
-    } else {
-      doContainer.appendChild(event.target.parentElement);
-    }
-  });
+addTaskBtn.addEventListener("click", (event) => {
+  todoList.appendChild(createListElement("item 1"));
 });
 
-function createListElement() {
+function handleCheck(event) {
+  const label = event.target.nextElementSibling;
+  if (event.target.checked) {
+    label.style.textDecoration = "line-through";
+    doneContainer.appendChild(event.target.parentElement);
+  } else {
+    label.style.textDecoration = "none";
+    doContainer.appendChild(event.target.parentElement);
+  }
+}
+
+function createListElement(label) {
   const liElem = document.createElement("li");
   const inputElem = document.createElement("input");
   const labelElem = document.createElement("label");
 
   inputElem.setAttribute("type", "checkbox");
   inputElem.setAttribute("name", "checkbox");
+  inputElem.addEventListener("click", handleCheck);
 
-  labelElem.textContent = "example label";
+  labelElem.textContent = label;
 
   liElem.appendChild(inputElem);
   liElem.appendChild(labelElem);
+
+  return liElem;
 }
